830eb2401f95fd7a6245db3d5d5adadaa8514d58
amaanansarii <amaanansari790786@gmail.com>
2025-12-29 07:58:24 +0530

    forth commit

---
+++ 01basics/DOM/dom1.js
@@ -0,0 +1,41 @@
+// const div = document.createElement('div')
+// div.className = 'main'
+// div.id = Math.round(Math.random() * 10 + 1)
+
+// div.setAttribute('title', 'generated-title')
+// div.style.backgroundColor = 'green'
+// div.style.padding = '12px'
+// div.innerText = 'anita pujari'
+// const addtest = document.createTextNode('amaan ansari')
+// div.appendChild(addtest)
+// // document.body.appendChild(div)
+
+// function addLanguage(lang){
+//     const li = document.createElement('li')
+//     li.innerHTML = `${lang}`
+//     document.querySelector('.language')
+//     .appendChild(li)
+// }
+
+// addLanguage("python")
+// addLanguage('tyscript')
+
+// function addoptimizedlanguage(langName){
+//     const li = document.createElement('li')
+//     li.appendChild(document.createTextNode(langName))
+//     document.querySelector('.language').appendChild(li)
+// }
+// addoptimizedlanguage('golang')
+
+// const secnodlang = document.querySelector('li:nth-child(2)')
+// console.log(secnodlang);
+
+// secnodlang.innerHTML = 'modo'
+// const newli = document.createElement('li')
+// secnodlang.replaceWith(newli)
+
+// const firstlang = document.querySelector('li:first-child')
+// firstlang.outerHTML = '<li> type script</li>'
+
+// const lastlang = document.querySelector('li: last-child')
+// lastlang.remove()
