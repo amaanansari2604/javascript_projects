5abce105fac939bf2e53168c8c200d2224ce9892
amaanansarii <amaanansari790786@gmail.com>
2026-01-09 07:34:40 +0530

    user model for ecommerce

---
+++ jsbackend/backend/models/ecommerce/user.model.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    }
+}, { timestamps: true})
+
+export const User = mongoose.model("User", userSchema);
