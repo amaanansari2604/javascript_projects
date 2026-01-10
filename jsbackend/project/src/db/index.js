43448c08b28f8120b161e84a3b889431b1058717
amaanansarii <amaanansari790786@gmail.com>
2026-01-10 14:32:55 +0530

    connecting db

---
+++ jsbackend/project/src/db/index.js
@@ -0,0 +1,26 @@
+//connecting database first approach
+
+import mongoose from "mongoose"
+import { DB_NAME } from "../contants"
+import express from "express";
+
+const app = express()
+(
+    async () => {
+        try {
+            
+            await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+            app.on("error", (error) => {
+                console.log("ERR", error);
+                throw error
+            })
+
+            app.listen(process.env.MONGODB_URI, () => {
+                console.log(`App is listening on port ${process.env.PORT}`)
+            });
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+)()
